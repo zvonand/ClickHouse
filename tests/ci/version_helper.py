a5367a1442e1779f3cb1e43d13b698dd538de1bb
Vasily Nemkov <V.Nemkov@gmail.com>
2025-03-06 09:05:59 +0000

    Debug logging

--- tests/ci/version_helper.py
+++ tests/ci/version_helper.py
@@ -60,6 +60,7 @@ class ClickHouseVersion:
         self._tweak = TWEAK
         if tweak is not None:
             self._tweak = int(tweak)
+            logging.info(f"!!!! set version tweak directly from argument {tweak} to {self._tweak}")
         elif self._git is not None:
             self._tweak = self._git.tweak
         self._describe = ""
@@ -307,32 +308,34 @@ def get_version_from_repo(
         tweak=versions.get("tweak", versions["revision"]),
         flavour=versions.get("flavour", None)
     )
+    logging.info(f"!!! Version from file: {cmake_version}")
 
     # if this commit is tagged, use tag's version instead of something stored in cmake
     if git is not None and git.latest_tag:
         version_from_tag = get_version_from_tag(git.latest_tag)
-        logging.debug(f'Git latest tag: {git.latest_tag} ({git.commits_since_latest} commits ago)\n'
+        logging.info(f'Git latest tag: {git.latest_tag} ({git.commits_since_latest} commits ago)\n'
             f'"new" tag: {git.new_tag} ({git.commits_since_new})\n'
             f'current commit: {git.sha}\n'
             f'current brach: {git.branch}'
         )
-        if git.commits_since_latest == 0:
+        if git.latest_tag and git.commits_since_latest == 0:
             # Tag has a priority over the version written in CMake.
             # Version must match (except tweak, flavour, description, etc.) to avoid accidental mess.
             if not (version_from_tag.major == cmake_version.major \
-                and version_from_tag.minor == cmake_version.minor \
-                and version_from_tag.patch == cmake_version.patch):
+                    and version_from_tag.minor == cmake_version.minor \
+                    and version_from_tag.patch == cmake_version.patch):
                 raise RuntimeError(f"Version generated from tag ({version_from_tag}) should have same major, minor, and patch values as version generated from cmake ({cmake_version})")
 
             # Don't need to reset version completely, mostly because revision part is not set in tag, but must be preserved
-            logging.debug(f"Resetting TWEAK and FLAVOUR of version from cmake {cmake_version} to values from tag: {version_from_tag.tweak}.{version_from_tag._flavour}")
+            logging.info(f"Resetting TWEAK and FLAVOUR of version from cmake {cmake_version} to values from tag: {version_from_tag.tweak}.{version_from_tag._flavour}")
             cmake_version._flavour = version_from_tag._flavour
             cmake_version.tweak = version_from_tag.tweak
         else:
             # We've had some number of commits since the latest (upstream) tag.
-            logging.debug(f"Bumping the TWEAK of version from cmake {cmake_version} by {git.commits_since_upstream}")
+            logging.info(f"Bumping the TWEAK of version from cmake {cmake_version} by {git.commits_since_upstream}")
             cmake_version.tweak = cmake_version.tweak + git.commits_since_upstream
 
+    logging.info(f"!!! Computed version: {cmake_version}")
     return cmake_version
 
 

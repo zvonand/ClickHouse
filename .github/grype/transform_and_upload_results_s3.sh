1d25532d40a4cae527c04a62b4213deb76763329
Stuart <146047128+strtgbb@users.noreply.github.com>
2025-03-11 14:49:58 -0400

    Add Grype results to ci_running

--- .github/grype/transform_and_upload_results_s3.sh
+++ .github/grype/transform_and_upload_results_s3.sh
@@ -1,9 +1,12 @@
 DOCKER_IMAGE=$(echo "$DOCKER_IMAGE" | sed 's/[\/:]/_/g')
 
-tfs --no-colors transform nice raw.log nice.log.txt
-tfs --no-colors report results -a "https://s3.amazonaws.com/$S3_BUCKET/$PR_NUMBER/$COMMIT_SHA/grype/$DOCKER_IMAGE" raw.log - --copyright "Altinity LTD" | tfs --no-colors document convert > results.html
-
 S3_PATH="s3://$S3_BUCKET/$PR_NUMBER/$COMMIT_SHA/grype/$DOCKER_IMAGE"
+HTTPS_S3_PATH="https://s3.amazonaws.com/$S3_BUCKET/$PR_NUMBER/$COMMIT_SHA/grype/$DOCKER_IMAGE"
+export HTTPS_S3_PATH
+
+tfs --no-colors transform nice raw.log nice.log.txt
+tfs --no-colors report results -a $HTTPS_S3_PATH raw.log - --copyright "Altinity LTD" | tfs --no-colors document convert > results.html
+
 aws s3 cp --no-progress nice.log.txt $S3_PATH/nice.log.txt --content-type "text/plain; charset=utf-8" || echo "nice log file not found".
 aws s3 cp --no-progress results.html $S3_PATH/results.html || echo "results file not found".
 aws s3 cp --no-progress raw.log $S3_PATH/raw.log || echo "raw.log file not found".

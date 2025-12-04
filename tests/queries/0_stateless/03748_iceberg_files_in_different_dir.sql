729849202de7f11a3e255423272852a29935dd77
Andrey Zvonov <zvonov.andrey@gmail.com>
2025-12-04 01:28:46 +0100

    Revert "add simplest test"
    This reverts commit 1dbe5252830ad543a09915feaaf718486d1c848b.

--- tests/queries/0_stateless/03748_iceberg_files_in_different_dir.sql
+++
@@ -1,4 +0,0 @@
--- Tags: no-fasttest
--- Tag no-fasttest: Depends on AWS
-
-SELECT * FROM icebergS3(s3_conn, filename='iceberg_split_data_test', SETTINGS iceberg_metadata_table_uuid = '149ecc15-7afc-4311-86b3-3a4c8d4ec08e') ORDER BY id;

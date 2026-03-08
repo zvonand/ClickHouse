91cc047cd4e6501621d3aae88a29809f4b9e4e1c
Onyx2406 <yashsancheti24@gmail.com>
2026-03-09 00:35:33 +0530

    Add test for TZ=:/etc/localtime file path syntax
    Shell test that verifies clickhouse-local works when TZ uses
    POSIX file path syntax with colon prefix, as requested by
    @alexey-milovidov in review.

---
+++ tests/queries/0_stateless/04041_tz_file_path_colon_prefix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+# Verify clickhouse-local works with POSIX TZ file path syntax (TZ=:/etc/localtime)
+# See https://github.com/ClickHouse/ClickHouse/issues/86495
+TZ=:/etc/localtime $CLICKHOUSE_LOCAL --query "SELECT 1"

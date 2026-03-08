8bdf00dceab016aad9ca13efe0ff9f6e1c031953
Matheus Nerone <matheus.agio@hotmail.com>
2026-03-08 14:37:02 +0100

    Adding tests for CSVWithNames

--- tests/queries/0_stateless/04028_csv_input_format.sh
+++ tests/queries/0_stateless/04028_csv_input_format.sh
@@ -6,7 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-
+# Base case for auto case
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String)"
 $CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
@@ -15,6 +15,16 @@ $CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
 $CLICKHOUSE_CLIENT -q "SELECT * FROM test"
 $CLICKHOUSE_CLIENT -q "DROP TABLE test"
 
+# Test ambiguity for automatic column name matching
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, iD Int, name String)"
+$CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
+                       SET input_format_with_names_case_insensitive_column_matching='auto';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames; -- { clientError 117 }"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Base case for match case
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String)"
 $CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
@@ -23,10 +33,64 @@ $CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
 $CLICKHOUSE_CLIENT -q "SELECT * FROM test"
 $CLICKHOUSE_CLIENT -q "DROP TABLE test"
 
+# Base case for ignore case
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (ID Int, NAME String)"
+$CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
+                       SET input_format_with_names_case_insensitive_column_matching='ignore_case';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names_no_duplicates.csv' FORMAT CSVWithNames;"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Test ambiguity for ignore case column name matching
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String)"
 $CLICKHOUSE_CLIENT -q "SET input_format_csv_detect_header=true;
                        SET input_format_with_names_case_insensitive_column_matching='ignore_case';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames; -- { clientError 117 }"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+
+# If the columns in the CSV don't match the columns of the table, detect header is called, which means
+# it is possible to avoid using input_format_csv_detect_header
+
+# Base case for auto case
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String, not_used Int)"
+$CLICKHOUSE_CLIENT -q "SET input_format_with_names_case_insensitive_column_matching='auto';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames;"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Test ambiguity for automatic column name matching
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, iD Int, name String, not_used Int)"
+$CLICKHOUSE_CLIENT -q "SET input_format_with_names_case_insensitive_column_matching='auto';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames; -- { clientError 117 }"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Base case for match case
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String, not_used Int)"
+$CLICKHOUSE_CLIENT -q "SET input_format_with_names_case_insensitive_column_matching='match_case';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Base case for ignore case
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (ID Int, NAME String, not_used Int)"
+$CLICKHOUSE_CLIENT -q "SET input_format_with_names_case_insensitive_column_matching='ignore_case';
+                       INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names_no_duplicates.csv' FORMAT CSVWithNames;"
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test"
+$CLICKHOUSE_CLIENT -q "DROP TABLE test"
+
+# Test ambiguity for ignore case column name matching
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test (id Int, ID Int, name String, not_used Int)"
+$CLICKHOUSE_CLIENT -q "SET input_format_with_names_case_insensitive_column_matching='ignore_case';
                        INSERT INTO test FROM INFILE '$CURDIR/data_csv/csv_with_names.csv' FORMAT CSVWithNames; -- { clientError 117 }"
 $CLICKHOUSE_CLIENT -q "SELECT * FROM test"
 $CLICKHOUSE_CLIENT -q "DROP TABLE test"

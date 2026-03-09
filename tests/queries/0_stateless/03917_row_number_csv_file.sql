93b746b7034e42a62a9f8e4dc88b0429d5b331e1
alesapin <alesapin@clickhouse.com>
2026-03-09 18:03:57 +0100

    Apply suggestions from code review

--- tests/queries/0_stateless/03917_row_number_csv_file.sql
+++ tests/queries/0_stateless/03917_row_number_csv_file.sql
@@ -1,8 +1,9 @@
+-- Tags: no-parallel
 -- Test _row_number virtual column for CSV format with and without parallel parsing
 
 DROP TABLE IF EXISTS test_csv_row_number;
 
-CREATE TABLE test_csv_row_number (a UInt32, b String) ENGINE = File('CSV', '${CLICKHOUSE_DATABASE}/data.csv');
+CREATE TABLE test_csv_row_number (a UInt32, b String) ENGINE = File(CSV);
 INSERT INTO test_csv_row_number VALUES (10, 'x'), (20, 'y'), (30, 'z'), (40, 'w'), (50, 'v');
 
 SELECT a, _row_number FROM test_csv_row_number ORDER BY a SETTINGS input_format_parallel_parsing = 0;

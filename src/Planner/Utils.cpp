a9376a143f3dd593955d3785403f7d7465493408
Dmitry Novik <mrnovikd@gmail.com>
2026-03-09 10:48:30 +0100

    Return the comment

--- src/Planner/Utils.cpp
+++ src/Planner/Utils.cpp
@@ -661,6 +661,9 @@ bool optimizePlanForExists(QueryPlan & query_plan)
         {
             if (limit_step->getOffset() == 0 && limit_step->getLimit() > 0)
             {
+                /// TODO: Support LimitStep in decorrelation process.
+                /// For now, we just remove it, because it only increases the number of rows in the result.
+                /// It doesn't affect the result of correlated subquery.
                 node = node->children[0];
                 continue;
             }

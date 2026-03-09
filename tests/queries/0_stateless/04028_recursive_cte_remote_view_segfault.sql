71c62d85a9f5f5ec98b5d65c3119fbcfde3ce9d8
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-03-09 21:19:21 +0100

    Enable analyzer explicitly

--- tests/queries/0_stateless/04028_recursive_cte_remote_view_segfault.sql
+++ tests/queries/0_stateless/04028_recursive_cte_remote_view_segfault.sql
@@ -3,6 +3,8 @@
 -- because isStorageUsedInTree tried to call getStorageID() on an unresolved
 -- view() TableFunctionNode whose storage was null.
 
+SET enable_analyzer=1;
+
 WITH RECURSIVE x AS (
     (SELECT 1 FROM remote('127.0.0.1', view(SELECT 1)))
     UNION ALL

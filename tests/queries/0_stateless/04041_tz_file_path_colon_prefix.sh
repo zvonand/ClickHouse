6d84784ea155d50e3804d15c19d2b01edaed6523
Onyx2406 <yashsancheti24@gmail.com>
2026-03-10 16:09:51 +0530

    fix: skip TZ test when /etc/localtime is missing

--- tests/queries/0_stateless/04041_tz_file_path_colon_prefix.sh
+++ tests/queries/0_stateless/04041_tz_file_path_colon_prefix.sh
@@ -7,4 +7,10 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Verify clickhouse-local works with POSIX TZ file path syntax (TZ=:/etc/localtime)
 # See https://github.com/ClickHouse/ClickHouse/issues/86495
+
+if [ ! -f /etc/localtime ]; then
+    echo 1
+    exit 0
+fi
+
 TZ=:/etc/localtime $CLICKHOUSE_LOCAL --query "SELECT 1"

288ff412cd9255ca6fd9e7674a6291a9bf93eb9b
Daniel Q <il9ue@users.noreply.github.com>
2026-03-10 09:24:30 +0100

    Apply reviewer feedback: use formatFilter and respect enable_read_write_grants flag

--- tests/queries/0_stateless/03594_system_grants_url_regexp.sql
+++ tests/queries/0_stateless/03594_system_grants_url_regexp.sql
@@ -1,6 +1,11 @@
 -- Tags: no-parallel
+
+-- Enable the setting to test the regex filter output
+SET access_control_improvements.enable_read_write_grants = 1;
+
 DROP USER IF EXISTS test_regex_grant_user;
 CREATE USER test_regex_grant_user;
+
 GRANT READ ON URL('http://localhost:912.*') TO test_regex_grant_user;
 GRANT READ ON S3('s3://foo/*') TO test_regex_grant_user;
 

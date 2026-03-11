c47e84ae0d8bef5de8b5c1a2fd68c4b93807f36d
Onyx2406 <yashsancheti24@gmail.com>
2026-03-12 01:17:55 +0530

    test: add mixed case with both ephemeral-dependent and regular MATERIALIZED columns

--- tests/queries/0_stateless/04044_mutation_ephemeral_materialized.sql
+++ tests/queries/0_stateless/04044_mutation_ephemeral_materialized.sql
@@ -23,3 +23,29 @@ ALTER TABLE t_ephemeral_materialized DELETE WHERE c2 = 'Hello World!';
 SELECT count() FROM t_ephemeral_materialized;
 
 DROP TABLE t_ephemeral_materialized;
+
+-- Mixed case: table with BOTH ephemeral-dependent AND non-ephemeral MATERIALIZED columns.
+-- Updating the source of the non-ephemeral one should recalculate it without
+-- trying to recalculate the ephemeral-dependent one.
+DROP TABLE IF EXISTS t_mixed_materialized;
+
+CREATE TABLE t_mixed_materialized
+(
+    c1 String EPHEMERAL,
+    c2 String MATERIALIZED tryBase64Decode(c1),
+    c3 String,
+    c4 String MATERIALIZED upper(c3)
+)
+ENGINE = MergeTree
+ORDER BY tuple();
+
+INSERT INTO t_mixed_materialized (c1, c3) VALUES ('SGVsbG8gV29ybGQh', 'hello');
+
+SELECT c2, c3, c4 FROM t_mixed_materialized;
+
+SET mutations_sync = 1;
+ALTER TABLE t_mixed_materialized UPDATE c3 = 'world' WHERE c2 = 'Hello World!';
+
+SELECT c2, c3, c4 FROM t_mixed_materialized;
+
+DROP TABLE t_mixed_materialized;

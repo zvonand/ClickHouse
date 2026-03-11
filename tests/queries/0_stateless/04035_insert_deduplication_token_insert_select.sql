e4e157ba39ac9cc8e99764cbcb08d90237644335
Desel72 <pedroluiscolmenares722@gmail.com>
2026-03-11 22:50:30 +0100

    Added new test file

--- tests/queries/0_stateless/04035_insert_deduplication_token_insert_select.sql
+++ tests/queries/0_stateless/04035_insert_deduplication_token_insert_select.sql
@@ -28,4 +28,28 @@ INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) ORDER BY
 INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) ORDER BY ALL SETTINGS insert_deduplication_token = 'token3';
 SELECT count() FROM t_dedup_token_insert_select;
 
+TRUNCATE TABLE t_dedup_token_insert_select;
+
+SELECT 'force_enable with non-empty token should succeed without ORDER BY ALL';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'force_enable', insert_deduplication_token = 'token4';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'force_enable', insert_deduplication_token = 'token4';
+SELECT count() FROM t_dedup_token_insert_select;
+
+SELECT 'force_enable with empty token and no ORDER BY ALL should raise DEDUPLICATION_IS_NOT_POSSIBLE';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'force_enable', insert_deduplication_token = ''; -- { serverError DEDUPLICATION_IS_NOT_POSSIBLE }
+
+TRUNCATE TABLE t_dedup_token_insert_select;
+
+SELECT 'enable_even_for_bad_queries with non-empty token should deduplicate';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'enable_even_for_bad_queries', insert_deduplication_token = 'token5';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'enable_even_for_bad_queries', insert_deduplication_token = 'token5';
+SELECT count() FROM t_dedup_token_insert_select;
+
+TRUNCATE TABLE t_dedup_token_insert_select;
+
+SELECT 'enable_even_for_bad_queries with empty token still deduplicates (unstable mode)';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'enable_even_for_bad_queries', insert_deduplication_token = '';
+INSERT INTO t_dedup_token_insert_select SELECT number FROM numbers(100) SETTINGS deduplicate_insert_select = 'enable_even_for_bad_queries', insert_deduplication_token = '';
+SELECT count() FROM t_dedup_token_insert_select;
+
 DROP TABLE t_dedup_token_insert_select SYNC;

bc415d0ec470b1603eba3ce7c6d51e825e28f63a
Igor Nikonov <igor@clickhouse.com>
2026-03-12 17:41:03 +0100

    Let's always check coordinator mode

--- src/Storages/MergeTree/ParallelReplicasReadingCoordinator.cpp
+++ src/Storages/MergeTree/ParallelReplicasReadingCoordinator.cpp
@@ -20,7 +20,6 @@
 #include <fmt/format.h>
 #include <Common/ElapsedTimeProfileEventIncrement.h>
 #include <Common/Exception.h>
-#include <Common/FailPoint.h>
 #include <Common/ProfileEvents.h>
 #include <Common/SipHash.h>
 #include <Common/logger_useful.h>
@@ -133,11 +132,6 @@ namespace ErrorCodes
 extern const int BAD_ARGUMENTS;
 extern const int LOGICAL_ERROR;
 extern const int ALL_CONNECTION_TRIES_FAILED;
-}
-
-namespace FailPoints
-{
-    extern const char parallel_replicas_check_read_mode_always[];
 }
 
 class ParallelReplicasReadingCoordinator::ImplInterface
@@ -1109,50 +1103,32 @@ void ParallelReplicasReadingCoordinator::handleInitialAllRangesAnnouncement(Init
     ProfileEvents::increment(ProfileEvents::ParallelReplicasNumRequests);
     ProfileEventTimeIncrement<Microseconds> watch(ProfileEvents::ParallelReplicasHandleAnnouncementMicroseconds);
 
-    const auto is_initialized = [this]() -> bool
-    {
-        std::lock_guard lock(mutex);
-        return pimpl.operator bool();
-    }();
+    std::lock_guard lock(mutex);
 
-    if (is_initialized)
+    if (!pimpl)
     {
-        fiu_do_on(FailPoints::parallel_replicas_check_read_mode_always, {
-            chassert(pimpl);
-            if (announcement.mode != pimpl->getCoordinationMode())
-            {
-                throw Exception(
-                    ErrorCodes::LOGICAL_ERROR,
-                    "Replica {} decided to read in {} mode, not in {}. This is a bug",
-                    announcement.replica_num,
-                    magic_enum::enum_name(announcement.mode),
-                    magic_enum::enum_name(pimpl->getCoordinationMode()));
-            }
-        });
+        initialize(announcement.mode);
+
+        chassert(!snapshot_replica_num);
+        snapshot_replica_num = announcement.replica_num;
+        LOG_DEBUG(getLogger("ParallelReplicasReadingCoordinator"), "Using snapshot from replica num {}", snapshot_replica_num.value());
+    }
+    else
+    {
+        // let's always check the reading mode match
+        if (announcement.mode != pimpl->getCoordinationMode())
+        {
+            throw Exception(
+                ErrorCodes::LOGICAL_ERROR,
+                "Replica {} decided to read in {} mode, not in {}. This is a bug",
+                announcement.replica_num,
+                magic_enum::enum_name(announcement.mode),
+                magic_enum::enum_name(pimpl->getCoordinationMode()));
+        }
     }
 
     if (is_reading_completed)
         return;
-
-    std::lock_guard lock(mutex);
-
-    if (!pimpl)
-        initialize(announcement.mode);
-
-    if (!snapshot_replica_num)
-    {
-        snapshot_replica_num = announcement.replica_num;
-
-        LOG_DEBUG(getLogger("ParallelReplicasReadingCoordinator"), "Using snapshot from replica num {}", snapshot_replica_num.value());
-    }
-
-    if (announcement.mode != pimpl->getCoordinationMode())
-        throw Exception(
-            ErrorCodes::LOGICAL_ERROR,
-            "Replica {} decided to read in {} mode, not in {}. This is a bug",
-            announcement.replica_num,
-            magic_enum::enum_name(announcement.mode),
-            magic_enum::enum_name(pimpl->getCoordinationMode()));
 
     pimpl->handleInitialAllRangesAnnouncement(std::move(announcement));
 }

45b8de293e2a72059b78344e6156e80cec2a5d01
Vitaly Baranov <vitlibar@clickhouse.com>
2026-03-12 13:28:15 +0100

    Fix hardcoded DateTime64 scale in function dataTypeToAST()

--- src/DataTypes/dataTypeToAST.cpp
+++ src/DataTypes/dataTypeToAST.cpp
@@ -5,6 +5,7 @@
 #include <DataTypes/DataTypeDateTime64.h>
 #include <DataTypes/DataTypesDecimal.h>
 #include <DataTypes/IDataType.h>
+#include <Common/DateLUTImpl.h>
 #include <Common/typeid_cast.h>
 #include <Parsers/ASTDataType.h>
 #include <Parsers/ASTLiteral.h>
@@ -23,7 +24,13 @@ boost::intrusive_ptr<ASTDataType> dataTypeToAST(const DataTypePtr & data_type)
         return makeASTDataType("Array", dataTypeToAST(typeid_cast<const DataTypeArray *>(data_type.get())->getNestedType()));
 
     if (which.isDateTime64())
-        return makeASTDataType("DateTime64", make_intrusive<ASTLiteral>(static_cast<UInt32>(6)));
+    {
+        const auto * dt64 = typeid_cast<const DataTypeDateTime64 *>(data_type.get());
+        auto scale = make_intrusive<ASTLiteral>(dt64->getScale());
+        if (dt64->hasExplicitTimeZone())
+            return makeASTDataType("DateTime64", scale, make_intrusive<ASTLiteral>(dt64->getTimeZone().getTimeZone()));
+        return makeASTDataType("DateTime64", scale);
+    }
 
     if (which.isDecimal())
         return makeASTDataType("Decimal", make_intrusive<ASTLiteral>(getDecimalPrecision(*data_type)), make_intrusive<ASTLiteral>(getDecimalScale(*data_type)));

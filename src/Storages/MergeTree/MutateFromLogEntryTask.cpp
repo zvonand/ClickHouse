30fb7cd14dad84bffae118678e3058f0efe7ab82
Raúl Marín <raul.marin@clickhouse.com>
2026-03-13 17:47:44 +0100

    Revert MutateFromLogEntryTask change: local variable prevents duplicates on retry

--- src/Storages/MergeTree/MutateFromLogEntryTask.cpp
+++ src/Storages/MergeTree/MutateFromLogEntryTask.cpp
@@ -139,10 +139,13 @@ ReplicatedMergeMutateTaskBase::PrepareResult MutateFromLogEntryTask::prepare()
         }
     }
 
-    /// mutation_ids_for_log can be empty here.
-    commands = std::make_shared<MutationCommands>(storage.queue.getMutationCommands(source_part, new_part_info.mutation, mutation_ids_for_log));
+    Strings mutation_ids;
+    commands = std::make_shared<MutationCommands>(storage.queue.getMutationCommands(source_part, new_part_info.mutation, mutation_ids));
     LOG_TRACE(log, "Mutating part {} with mutation commands from {} mutations ({}): {}",
-              entry.new_part_name, commands->size(), fmt::join(mutation_ids_for_log, ", "), commands->toString(true));
+              entry.new_part_name, commands->size(), fmt::join(mutation_ids, ", "), commands->toString(true));
+
+    /// mutation_ids can be empty here.
+    mutation_ids_for_log = mutation_ids;
 
     /// Once we mutate part, we must reserve space on the same disk, because mutations can possibly create hardlinks.
     /// Can throw an exception.

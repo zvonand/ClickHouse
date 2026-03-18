afde21845a213bca6a58377bab20b4d2a3d92154
Sema Checherinda <checherinda@gmail.com>
2026-03-18 14:53:54 +0100

    adjust the tests

--- tests/queries/0_stateless/04046_async_insert_progress_stats.sh
+++ tests/queries/0_stateless/04046_async_insert_progress_stats.sh
@@ -12,54 +12,32 @@ ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test_async_insert_progress"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE test_async_insert_progress (x String) ENGINE = MergeTree ORDER BY x"
 
 # Test 1: TCP protocol (clickhouse-client uses TCP)
-${CLICKHOUSE_CLIENT} --async_insert 1 --wait_for_async_insert 1 --query \
+query_tcp_id="ASYNC_INSERT_TCP_$RANDOM$RANDOM"
+${CLICKHOUSE_CLIENT} --query_id="$query_tcp_id" --async_insert 1 --wait_for_async_insert 1 --query \
     "INSERT INTO test_async_insert_progress VALUES ('one'), ('two'), ('three')"
 
-${CLICKHOUSE_CLIENT} --query "SYSTEM FLUSH LOGS"
-
-${CLICKHOUSE_CLIENT} --query "
-    SELECT 'TCP written_rows', written_rows
-    FROM system.query_log
-    WHERE current_database = currentDatabase()
-        AND query LIKE 'INSERT INTO test_async_insert_progress%'
-        AND type = 'QueryFinish'
-        AND interface = 'TCP'
-    ORDER BY event_time_microseconds DESC
-    LIMIT 1"
-
-${CLICKHOUSE_CLIENT} --query "
-    SELECT 'TCP result_rows', result_rows
-    FROM system.query_log
-    WHERE current_database = currentDatabase()
-        AND query LIKE 'INSERT INTO test_async_insert_progress%'
-        AND type = 'QueryFinish'
-        AND interface = 'TCP'
-    ORDER BY event_time_microseconds DESC
-    LIMIT 1"
-
 # Test 2: HTTP protocol
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&async_insert=1&wait_for_async_insert=1" \
+query_http_id="ASYNC_INSERT_HTTP_$RANDOM$RANDOM"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&async_insert=1&wait_for_async_insert=1&query_id=$query_http_id" \
     -d "INSERT INTO test_async_insert_progress VALUES ('four'), ('five'), ('six')"
 
-${CLICKHOUSE_CLIENT} --query "SYSTEM FLUSH LOGS"
+${CLICKHOUSE_CLIENT} --query "SYSTEM FLUSH LOGS query_log"
 
 ${CLICKHOUSE_CLIENT} --query "
-    SELECT 'HTTP written_rows', written_rows
+    SELECT 'TCP:', read_rows, read_bytes, written_rows, written_bytes, result_rows, result_bytes, interface
     FROM system.query_log
     WHERE current_database = currentDatabase()
-        AND query LIKE 'INSERT INTO test_async_insert_progress%'
+        AND query_id = '$query_tcp_id'
         AND type = 'QueryFinish'
-        AND interface = 'HTTP'
     ORDER BY event_time_microseconds DESC
     LIMIT 1"
 
 ${CLICKHOUSE_CLIENT} --query "
-    SELECT 'HTTP result_rows', result_rows
+    SELECT 'HTTP:', read_rows, read_bytes, written_rows, written_bytes, result_rows, result_bytes, interface
     FROM system.query_log
     WHERE current_database = currentDatabase()
-        AND query LIKE 'INSERT INTO test_async_insert_progress%'
+        AND query_id = '$query_http_id'
         AND type = 'QueryFinish'
-        AND interface = 'HTTP'
     ORDER BY event_time_microseconds DESC
     LIMIT 1"
 

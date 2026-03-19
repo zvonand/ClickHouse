4037d6a3bc6b85dbc15f41d54f86517cadc2b6ab
Takumi Hara <takumihara1226@gmail.com>
2026-03-19 11:56:07 +0900

    Fix failing tests for `toWeek` non-monotonicity change
    - Add missing `GROUP BY` to `03732_toweek_partition_pruning` test
    - Update `03789_to_year_week_monotonicity_key_condition` to verify
      that `toWeek` can no longer use primary key index (`INDEX_NOT_USED`)

--- tests/queries/0_stateless/03732_toweek_partition_pruning.sql
+++ tests/queries/0_stateless/03732_toweek_partition_pruning.sql
@@ -15,7 +15,7 @@ INSERT INTO test_toweek_pruning VALUES
     ('2025-12-21', 'x'), ('2025-12-22', 'x'), ('2025-12-28', 'x'),
     ('2025-12-29', 'x'), ('2025-12-31', 'x');
 
-SELECT toWeek(date, 3), count() FROM test_toweek_pruning WHERE toWeek(date, 3) = 49;
-SELECT toWeek(date, 3), count() FROM test_toweek_pruning WHERE toWeek(date, 3) = 52;
+SELECT toWeek(date, 3), count() FROM test_toweek_pruning WHERE toWeek(date, 3) = 49 GROUP BY 1;
+SELECT toWeek(date, 3), count() FROM test_toweek_pruning WHERE toWeek(date, 3) = 52 GROUP BY 1;
 
 DROP TABLE test_toweek_pruning;

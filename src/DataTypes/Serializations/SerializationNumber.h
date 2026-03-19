c9742bcc0a40edfc90dfa9ead23b67949e74e29f
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2026-03-19 17:33:21 +0100

    More cosmetics

--- src/DataTypes/Serializations/SerializationNumber.h
+++ src/DataTypes/Serializations/SerializationNumber.h
@@ -23,10 +23,7 @@ public:
     using ColumnType = ColumnVector<T>;
 
     static UInt128 getHash();
-
     static SerializationPtr create();
-
-    size_t allocatedBytes() const override;
 
     void serializeText(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
     void deserializeText(IColumn & column, ReadBuffer & istr, const FormatSettings & settings, bool whole) const override;

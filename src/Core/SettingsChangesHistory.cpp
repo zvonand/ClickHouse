8457ed610d0d5a5c3f020fe0ffaf6248290cc1a4
Dmitry Novik <n0vik@clickhouse.com>
2026-03-20 12:17:40 +0100

    Update setting changes history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -41,7 +41,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// Note: please check if the key already exists to prevent duplicate entries.
         addSettingsChanges(settings_changes_history, "26.4",
         {
-
+            {"enable_materialized_cte", false, false, "New setting"},
         });
         addSettingsChanges(settings_changes_history, "26.3",
         {

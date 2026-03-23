9fdbaeef5d759971e58b9b4a54b6bb39d6eea4c2
Arsen Muk <arsenmuk@clickhouse.com>
2026-03-23 12:03:01 +0000

    Fix lost logic of 00156_max_execution_speed_sample_merge

--- tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
+++ tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
@@ -1,7 +1,6 @@
--- Tags: stateful
 -- This test validates that max_execution_speed & timeout_before_checking_execution_speed works with:
 --      a) regular query
---      b) temp merge query
+--      b) merge query
 -- NOTE: This test uses simple synthetic data to validate the fact throttling was applied.
 -- If throttling works as expected - each execution will take >= 1 second, as we allow not more than {max_execution_speed} records/seconds
 -- If it doesn't - each select will finish immediately, and the test will fail
@@ -14,18 +13,26 @@ SET max_block_size = 1;
 CREATE TEMPORARY TABLE times (t DateTime);
 
 DROP TABLE IF EXISTS t00156_max_execution_speed_sample_merge;
-CREATE TABLE t00156_max_execution_speed_sample_merge (v UInt64);
-INSERT INTO t00156_max_execution_speed_sample_merge SELECT number FROM numbers(28);
+CREATE 
+  TABLE t00156_max_execution_speed_sample_merge
+    (v UInt64)
+  ENGINE = MergeTree
+  ORDER BY intHash32(v)
+  SAMPLE BY intHash32(v);
+
+INSERT INTO t00156_max_execution_speed_sample_merge SELECT number FROM numbers(4);
 
 INSERT INTO times SELECT now();
-SELECT * FROM t00156_max_execution_speed_sample_merge WHERE sleepEachRow(0.1) == 0 FORMAT Null;
+SELECT * FROM t00156_max_execution_speed_sample_merge SAMPLE 1/2 WHERE sleepEachRow(1) == 0 FORMAT Null;
 INSERT INTO times SELECT now();
 
-SELECT max(t) - min(t) >= 1 FROM times;
+SELECT max(t) - min(t) >= 2 FROM times;
 TRUNCATE TABLE times;
 
 INSERT INTO times SELECT now();
-SELECT * FROM t00156_max_execution_speed_sample_merge WHERE sleepEachRow(0.1) == 0 FORMAT Null;
+SELECT * FROM merge('t00156_max_execution_speed_sample_merge') SAMPLE 1/2 WHERE sleepEachRow(1) == 0 FORMAT Null;
 INSERT INTO times SELECT now();
 
-SELECT max(t) - min(t) >= 1 FROM times;
+SELECT max(t) - min(t) >= 2 FROM times;
+
+DROP TABLE IF EXISTS t00156_max_execution_speed_sample_merge;

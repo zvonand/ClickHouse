a4d64ca709ffe40624f147c3fa43f447d6981ace
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-03-23 14:04:38 +0000

    The new analyzer is required for this test

--- tests/queries/0_stateless/04054_block_mismatch.sql
+++ tests/queries/0_stateless/04054_block_mismatch.sql
@@ -1,3 +1,4 @@
+SET enable_analyzer = 1; --new analyzer is required for QUALIFY
 DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 Int, c1 Int) ENGINE = ReplacingMergeTree(c1) ORDER BY c0;
 INSERT INTO t0 VALUES (1, 1);

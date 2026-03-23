6985188ae4bad1d2b79d6b0cc522bec2258d75e3
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-03-23 10:48:08 +0000

    update test

--- tests/queries/0_stateless/04053_alter_command_segments_validation.sql
+++ tests/queries/0_stateless/04053_alter_command_segments_validation.sql
@@ -1,3 +1,6 @@
+-- Tags: no-replicated-database
+-- Tag no-replicated-database: Test uses combination of different alter segments
+
 DROP TABLE IF EXISTS t_alter_segments;
 
 CREATE TABLE t_alter_segments (i Int32) ENGINE = MergeTree ORDER BY i;
@@ -5,8 +8,8 @@ INSERT INTO t_alter_segments VALUES (1), (2), (3);
 
 ALTER TABLE t_alter_segments ADD COLUMN s String DEFAULT toString(i*i);
 ALTER TABLE t_alter_segments
-    MATERIALIZE COLUMN s,
-    RENAME COLUMN s TO s2
+    MATERIALIZE COLUMN s,  -- segment 1: mutation
+    RENAME COLUMN s TO s2  -- segment 2: alter
     SETTINGS mutations_sync = 2, alter_sync = 2;
 
 SELECT i, s2 FROM t_alter_segments ORDER BY i;

ab1082f98d254eb71ba36afd390825baf233bedf
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2026-03-24 16:43:05 +0000

    Fix mock

--- tests/integration/test_storage_s3/s3_mocks/gcs_transcode_mock.py
+++ tests/integration/test_storage_s3/s3_mocks/gcs_transcode_mock.py
@@ -1,35 +1,41 @@
-import gzip
 import sys
-
-from bottle import request, response, route, run
+import http.server
 
 DATA = b'{"id":1}\n{"id":2}\n{"id":3}\n'
-COMPRESSED = gzip.compress(DATA)
 
 
-@route("/")
-def ping():
-    response.content_type = "text/plain"
-    response.set_header("Content-Length", 2)
-    return "OK"
+class Handler(http.server.BaseHTTPRequestHandler):
+    protocol_version = "HTTP/1.1"
+
+    def do_HEAD(self):
+        self.send_response(200)
+        # No Content-Length: simulates GCS decompressive transcoding
+        self.send_header("Content-Type", "application/octet-stream")
+        self.send_header("ETag", '"abc123"')
+        self.send_header("Last-Modified", "Fri, 13 Mar 2026 10:54:50 GMT")
+        self.send_header("Connection", "close")
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Connection", "close")
+            self.end_headers()
+            self.wfile.write(b"OK")
+            return
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/octet-stream")
+        # No Content-Length on GET either
+        self.send_header("ETag", '"abc123"')
+        self.send_header("Last-Modified", "Fri, 13 Mar 2026 10:54:50 GMT")
+        self.send_header("Connection", "close")
+        self.end_headers()
+        self.wfile.write(DATA)
+
+    def log_message(self, *args):
+        pass
 
 
-@route("/<_bucket>/<_path:path>", method="HEAD")
-def head(_bucket, _path):
-    response.content_type = "application/octet-stream"
-    # No Content-Length: simulates GCS decompressive transcoding
-    response.set_header("ETag", '"abc123"')
-    response.set_header("Last-Modified", "Fri, 13 Mar 2026 10:54:50 GMT")
-    return ""
-
-
-@route("/<_bucket>/<_path:path>", method="GET")
-def get(_bucket, _path):
-    response.content_type = "application/octet-stream"
-    # No Content-Length on GET either
-    response.set_header("ETag", '"abc123"')
-    response.set_header("Last-Modified", "Fri, 13 Mar 2026 10:54:50 GMT")
-    return DATA
-
-
-run(host="0.0.0.0", port=int(sys.argv[1]))
+http.server.HTTPServer(("0.0.0.0", int(sys.argv[1])), Handler).serve_forever()

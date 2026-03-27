9e36d151c2a50b1860af8b0b3d3a448c831c678c
Anton Popov <anton@clickhouse.com>
2026-03-27 17:47:16 +0100

    fix style check

--- src/DataTypes/DataTypesCache.cpp
+++ src/DataTypes/DataTypesCache.cpp
@@ -4,6 +4,11 @@
 namespace DB
 {
 
+namespace ErrorCodes
+{
+    extern const int LOGICAL_ERROR;
+}
+
 SimpleDataTypesCache::SimpleDataTypesCache()
 {
     addSimpleType(BinaryTypeIndex::Nothing, "Nothing");

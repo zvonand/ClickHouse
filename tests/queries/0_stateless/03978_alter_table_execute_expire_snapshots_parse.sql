3bc6891f95cac5070b6468e93e8e851c76d7125a
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-03-28 12:12:57 +0000

    disable replicated database

--- tests/queries/0_stateless/03978_alter_table_execute_expire_snapshots_parse.sql
+++ tests/queries/0_stateless/03978_alter_table_execute_expire_snapshots_parse.sql
@@ -1,3 +1,5 @@
+-- Tags: no-replicated-database
+
 -- Parsing: expire_snapshots with positional timestamp (legacy syntax)
 SELECT formatQuerySingleLine('ALTER TABLE t EXECUTE expire_snapshots(\'2024-06-01 00:00:00\')');
 SELECT formatQuerySingleLine('ALTER TABLE db.t EXECUTE expire_snapshots(\'2024-06-01 00:00:00\')');

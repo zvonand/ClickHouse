15e60c1beaf567c6c9182cdd3b8fe6d1e87b2334
Aruj Bansal <arujbansal1@gmail.com>
2026-03-29 12:58:41 -0400

    Cover more test cases for trailing comma in WITH clause

--- tests/queries/0_stateless/04065_with_trailing_comma.sql
+++ tests/queries/0_stateless/04065_with_trailing_comma.sql
@@ -11,8 +11,16 @@ WITH (SELECT 1) AS a, SELECT a;
 WITH 1 AS a SELECT a;
 WITH 1 AS a, 2 AS b SELECT a + b;
 
--- Multiple WITH elements, mixed expressions, trailing comma
-WITH 1 AS a, (SELECT 2) AS b, 3 AS c, SELECT a + b + c;
+-- Named CTE with trailing comma
+WITH cte AS (SELECT number FROM numbers(3)), SELECT * FROM cte;
+
+-- Mix of named CTE and scalar expression with trailing comma
+WITH 1 AS a, cte AS (SELECT number FROM numbers(3)), SELECT a + sum(number) FROM cte;
+
+-- Materialized CTE with trailing comma
+SET enable_materialized_cte = 1;
+WITH cte AS MATERIALIZED (SELECT number FROM numbers(3)), SELECT * FROM cte;
+SET enable_materialized_cte = 0;
 
 -- Trailing comma in RECURSIVE WITH
 WITH RECURSIVE recursive_cte AS (SELECT 1 AS n UNION ALL SELECT n + 1 FROM recursive_cte WHERE n < 3), SELECT * FROM recursive_cte;
@@ -21,7 +29,7 @@ WITH RECURSIVE recursive_cte AS (SELECT 1 AS n UNION ALL SELECT n + 1 FROM recur
 SELECT * FROM (WITH 1 AS a, SELECT a);
 
 -- Multiple trailing-comma WITH clauses in UNION
-WITH 1 AS a, SELECT a UNION ALL WITH 2 AS b, SELECT b;
+SELECT * FROM (WITH 1 AS a, SELECT a UNION ALL WITH 2 AS b, SELECT b) ORDER BY 1;
 
 -- Double trailing comma should fail
 WITH 1 AS a,, SELECT a; -- { serverError SYNTAX_ERROR }

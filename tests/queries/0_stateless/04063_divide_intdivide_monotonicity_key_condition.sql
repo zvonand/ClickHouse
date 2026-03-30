76be31033e2c36cc01cf7a47194b2c4e65c6d83f
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-03-30 19:02:21 +0000

    Add new case to the test

--- tests/queries/0_stateless/04063_divide_intdivide_monotonicity_key_condition.sql
+++ tests/queries/0_stateless/04063_divide_intdivide_monotonicity_key_condition.sql
@@ -20,3 +20,12 @@ SELECT * FROM test_divide WHERE x != 0 AND divide(10, x) > 4 ORDER BY x;
 
 EXPLAIN indexes = 1
 SELECT * FROM test_divide WHERE x != 0 AND divide(10, x) > 4 ORDER BY x;
+
+-- Test variable / const case: divide(x, 0) is undefined, MinMax must not prune partitions
+DROP TABLE IF EXISTS test_div_by_zero_const;
+CREATE TABLE test_div_by_zero_const (x Int32) ENGINE = MergeTree PARTITION BY x ORDER BY tuple();
+INSERT INTO test_div_by_zero_const VALUES (-2), (-1), (0), (1), (2);
+
+-- divide(x, 0) is undefined so MinMax index must read all 5 parts (no pruning)
+EXPLAIN indexes = 1
+SELECT * FROM test_div_by_zero_const WHERE divide(x, 0) > 0;

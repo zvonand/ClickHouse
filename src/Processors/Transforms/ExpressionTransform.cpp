4f66c2fa1ef09e9cbd055c385d40568dc432420f
Roman Vasin <r.vasin@arenadata.io>
2026-04-01 12:19:36 +0000

    Revert "Add more logging"
    This reverts commit 573907cd3de3e0767db163524b14b0ca42ca05ba.

--- src/Processors/Transforms/ExpressionTransform.cpp
+++ src/Processors/Transforms/ExpressionTransform.cpp
@@ -22,7 +22,6 @@ ExpressionTransform::ExpressionTransform(
     , expression(std::move(expression_))
     , updater(std::move(updater_))
 {
-    LOG_DEBUG(getLogger("ExpressionTransform"), "ExpressionTransform() enter this={}, expression={}", static_cast<const void*>(this), static_cast<void*>(expression.get()));
 }
 
 void ExpressionTransform::transform(Chunk & chunk)

f0d14cf601fdfe09e0a2b28d0d48f2f8877ce48f
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-04-01 16:46:04 +0000

    support common virtuals for mt

--- src/Processors/QueryPlan/ReadFromMergeTree.cpp
+++ src/Processors/QueryPlan/ReadFromMergeTree.cpp
@@ -3331,6 +3331,7 @@ void ReadFromMergeTree::initializePipeline(QueryPipelineBuilder & pipeline, cons
     }
 
     shared_virtual_fields.emplace("_sample_factor", result.sampling.used_sample_factor);
+    shared_virtual_fields.emplace("_table", data.getStorageID().getTableName());
 
     LOG_DEBUG(
         log,

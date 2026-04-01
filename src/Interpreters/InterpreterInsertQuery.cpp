ac121badd937a6fba0f7aa91cde31f0944416c55
Konstantin Vedernikov <75157521+scanhex12@users.noreply.github.com>
2026-04-01 17:53:15 +0200

    Update InterpreterInsertQuery.cpp

--- src/Interpreters/InterpreterInsertQuery.cpp
+++ src/Interpreters/InterpreterInsertQuery.cpp
@@ -863,10 +863,12 @@ std::optional<QueryPipeline> InterpreterInsertQuery::distributedWriteIntoReplica
         {
             try
             {
-                auto columns = src_storage_cluster->getInMemoryMetadataPtr()->getColumns().getAll();
+                const auto metadata = src_storage_cluster->getInMemoryMetadataPtr();
+                const auto snapshot = src_storage_cluster->getStorageSnapshot(metadata, local_context);
+                const auto columns = snapshot->getColumns(GetColumnsOptions(GetColumnsOptions::All).withVirtuals());
                 auto syntax = TreeRewriter(local_context).analyze(condition_ast, columns);
                 filter_dag = ExpressionAnalyzer(condition_ast, syntax, local_context).getActionsDAG(true, true);
-                predicate = filter_dag->getOutputs().at(0);
+                predicate = filter_dag->getOutputs().at(0);            
             }
             catch (...)
             {

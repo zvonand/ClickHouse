a9bd49755316d034c630ae8822356f50dce25dbb
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2026-04-06 11:18:34 +0000

    Remove unnecessary comments

--- tests/queries/0_stateless/04073_backup_from_snapshot_ast_format.sql
+++ tests/queries/0_stateless/04073_backup_from_snapshot_ast_format.sql
@@ -1,7 +1,3 @@
--- Test that BACKUP FROM SNAPSHOT AST round-trips correctly through format and parse.
--- Previously, formatQueryImpl did not emit the FROM SNAPSHOT clause, causing
--- "Inconsistent AST formatting" server exceptions.
-
 SELECT formatQuery('BACKUP FROM SNAPSHOT S3(\'http://localhost:9000/bucket/snapshot/\') TO S3(\'http://localhost:9000/bucket/backup/\')');
 SELECT formatQuery('BACKUP FROM SNAPSHOT S3(\'http://localhost:9000/bucket/snapshot/\') TO S3(\'http://localhost:9000/bucket/backup/\') SETTINGS id = \'abc\'');
 SELECT formatQuery('BACKUP FROM SNAPSHOT Disk(\'default\', \'/snapshot/\') TO Disk(\'default\', \'/backup/\')');

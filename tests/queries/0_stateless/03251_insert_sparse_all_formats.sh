e8610cf71a6837f2c08c32fe0c69573d5bbc8cdc
Alexey Milovidov <milovidov@clickhouse.com>
2026-04-07 20:36:13 +0200

    Revert "Disable parallel replicas for test `03251_insert_sparse_all_formats`"
    This reverts commit ed50bf9736bc2be84a6773c338f9a4edf6fd653a.

--- tests/queries/0_stateless/03251_insert_sparse_all_formats.sh
+++ tests/queries/0_stateless/03251_insert_sparse_all_formats.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, long, no-msan, no-azure-blob-storage, no-parallel-replicas
+# Tags: no-fasttest, long, no-msan, no-azure-blob-storage
 # no-azure-blob-storage: too slow
 # no-msan: it is too slow
-# no-parallel-replicas: the SELECT queries piped to INSERT hang with parallel replicas under TSan
 
 set -e
 

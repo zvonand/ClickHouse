182709c296923474b0684c3f1aeb27f1048edbe1
Robert Schulze <robert@clickhouse.com>
2026-04-07 14:44:59 +0000

    Cosmetics

--- src/Functions/padString.cpp
+++ src/Functions/padString.cpp
@@ -2,6 +2,7 @@
 #include <Common/StringUtils.h>
 #include <Columns/ColumnString.h>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/IDataType.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
 #include <Functions/GatherUtils/Algorithms.h>
@@ -24,7 +25,7 @@ namespace ErrorCodes
 namespace
 {
     /// The maximum new padded length.
-    constexpr ssize_t MAX_NEW_LENGTH = 1000000;
+    constexpr ssize_t MAX_NEW_LENGTH = 1'000'000;
 
     /// Appends padding characters to a sink based on a pad string.
     /// Depending on how many padding characters are required to add
@@ -33,48 +34,9 @@ namespace
     class PaddingChars
     {
     public:
-        explicit PaddingChars(const String & pad_string_) : pad_string(pad_string_) { init(); }
-
-        ALWAYS_INLINE size_t numCharsInPadString() const
+        explicit PaddingChars(const String & pad_string_)
+            : pad_string(pad_string_)
         {
-            if constexpr (is_utf8)
-                return utf8_offsets.size() - 1;
-            else
-                return pad_string.length();
-        }
-
-        ALWAYS_INLINE size_t numCharsToNumBytes(size_t count) const
-        {
-            if constexpr (is_utf8)
-                return utf8_offsets[count];
-            else
-                return count;
-        }
-
-        void appendTo(StringSink & res_sink, size_t num_chars) const
-        {
-            if (!num_chars)
-                return;
-
-            const size_t step = numCharsInPadString();
-            while (true)
-            {
-                if (num_chars <= step)
-                {
-                    writeSlice(StringSource::Slice{reinterpret_cast<const UInt8 *>(pad_string.data()), numCharsToNumBytes(num_chars)}, res_sink);
-                    break;
-                }
-                writeSlice(StringSource::Slice{reinterpret_cast<const UInt8 *>(pad_string.data()), numCharsToNumBytes(step)}, res_sink);
-                num_chars -= step;
-            }
-        }
-
-    private:
-        void init()
-        {
-            if (pad_string.empty())
-                pad_string = " ";
-
             if constexpr (is_utf8)
             {
                 size_t offset = 0;
@@ -107,6 +69,41 @@ namespace
             }
         }
 
+        ALWAYS_INLINE size_t numCharsInPadString() const
+        {
+            if constexpr (is_utf8)
+                return utf8_offsets.size() - 1;
+            else
+                return pad_string.length();
+        }
+
+        ALWAYS_INLINE size_t numCharsToNumBytes(size_t count) const
+        {
+            if constexpr (is_utf8)
+                return utf8_offsets[count];
+            else
+                return count;
+        }
+
+        void appendTo(StringSink & res_sink, size_t num_chars) const
+        {
+            if (num_chars == 0)
+                return;
+
+            const size_t step = numCharsInPadString();
+            while (true)
+            {
+                if (num_chars <= step)
+                {
+                    writeSlice(StringSource::Slice{reinterpret_cast<const UInt8 *>(pad_string.data()), numCharsToNumBytes(num_chars)}, res_sink);
+                    break;
+                }
+                writeSlice(StringSource::Slice{reinterpret_cast<const UInt8 *>(pad_string.data()), numCharsToNumBytes(step)}, res_sink);
+                num_chars -= step;
+            }
+        }
+
+    private:
         String pad_string;
 
         /// Offsets of code points in `pad_string`:
@@ -145,7 +142,10 @@ namespace
     {
     public:
         FunctionPadString(const char * name_, bool is_right_pad_, bool is_utf8_)
-            : function_name(name_), is_right_pad(is_right_pad_), is_utf8(is_utf8_) {}
+            : function_name(name_)
+            , is_right_pad(is_right_pad_)
+            , is_utf8(is_utf8_)
+        {}
 
         static FunctionPtr create(const char * name, bool is_right_pad, bool is_utf8)
         {
@@ -161,37 +161,17 @@ namespace
 
         bool useDefaultImplementationForConstants() const override { return false; }
 
-        DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+        DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
         {
-            size_t number_of_arguments = arguments.size();
+            FunctionArgumentDescriptors mandatory_args{
+                {"string", static_cast<FunctionArgumentDescriptor::TypeValidator>(&isStringOrFixedString), nullptr, "Array"},
+                {"length", static_cast<FunctionArgumentDescriptor::TypeValidator>(&isInteger), nullptr, "const UInt*"},
+            };
+            FunctionArgumentDescriptors optional_args{
+                {"pad_string", static_cast<FunctionArgumentDescriptor::TypeValidator>(&isStringOrFixedString), isColumnConst, "Array"}
+            };
 
-            if (number_of_arguments != 2 && number_of_arguments != 3)
-                throw Exception(
-                    ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
-                    "Number of arguments for function {} doesn't match: passed {}, should be 2 or 3",
-                    getName(),
-                    number_of_arguments);
-
-            if (!isStringOrFixedString(arguments[0]))
-                throw Exception(
-                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
-                    "Illegal type {} of the first argument of function {}, should be string",
-                    arguments[0]->getName(),
-                    getName());
-
-            if (!isInteger(arguments[1]))
-                throw Exception(
-                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
-                    "Illegal type {} of the second argument of function {}, should be unsigned integer",
-                    arguments[1]->getName(),
-                    getName());
-
-            if (number_of_arguments == 3 && !isStringOrFixedString(arguments[2]))
-                throw Exception(
-                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
-                    "Illegal type {} of the third argument of function {}, should be const string",
-                    arguments[2]->getName(),
-                    getName());
+            validateFunctionArguments(*this, arguments, mandatory_args, optional_args);
 
             return std::make_shared<DataTypeString>();
         }
@@ -211,15 +191,11 @@ namespace
             {
                 auto column_pad = arguments[2].column;
                 const ColumnConst * column_pad_const = checkAndGetColumnConst<ColumnString>(column_pad.get());
-                if (!column_pad_const)
-                    throw Exception(
-                        ErrorCodes::ILLEGAL_COLUMN,
-                        "Illegal column {}, third argument of function {} must be a constant string",
-                        column_pad->getName(),
-                        getName());
-
+                chassert(column_pad_const);
                 pad_string = column_pad_const->getValue<String>();
             }
+            if (pad_string.empty())
+                pad_string = " ";
 
             auto col_res = ColumnString::create();
             StringSink res_sink{*col_res, input_rows_count};
@@ -233,11 +209,7 @@ namespace
             else if (const ColumnConst * col_const_fixed = checkAndGetColumnConst<ColumnFixedString>(column_string.get()))
                 executeForSource(ConstSource<FixedStringSource>{*col_const_fixed}, column_length, pad_string, res_sink);
             else
-                throw Exception(
-                    ErrorCodes::ILLEGAL_COLUMN,
-                    "Illegal column {}, first argument of function {} must be a string",
-                    arguments[0].column->getName(),
-                    getName());
+                chassert(false);
 
             return col_res;
         }
@@ -248,29 +220,38 @@ namespace
         {
             const auto & chars = strings.getElements();
             bool all_ascii = isAllASCII(reinterpret_cast<const UInt8 *>(pad_string.data()), pad_string.size())
-                && isAllASCII(chars.data(), chars.size());
+                                && isAllASCII(chars.data(), chars.size());
             bool is_actually_utf8 = is_utf8 && !all_ascii;
 
             if (!is_actually_utf8)
             {
                 PaddingChars<false> padding_chars{pad_string};
                 if (const auto * col_const = checkAndGetColumn<ColumnConst>(column_length.get()))
-                    executeForSourceAndLength<false>(
-                        std::forward<SourceStrings>(strings), ConstSource<GenericValueSource>{*col_const}, padding_chars, res_sink);
+                    executeForSourceAndLength<false>(std::forward<SourceStrings>(strings), ConstSource<GenericValueSource>{*col_const}, padding_chars, res_sink);
                 else
-                    executeForSourceAndLength<false>(
-                        std::forward<SourceStrings>(strings), GenericValueSource{*column_length}, padding_chars, res_sink);
+                    executeForSourceAndLength<false>(std::forward<SourceStrings>(strings), GenericValueSource{*column_length}, padding_chars, res_sink);
             }
             else
             {
                 PaddingChars<true> padding_chars{pad_string};
                 if (const auto * col_const = checkAndGetColumn<ColumnConst>(column_length.get()))
-                    executeForSourceAndLength<true>(
-                        std::forward<SourceStrings>(strings), ConstSource<GenericValueSource>{*col_const}, padding_chars, res_sink);
+                    executeForSourceAndLength<true>(std::forward<SourceStrings>(strings), ConstSource<GenericValueSource>{*col_const}, padding_chars, res_sink);
                 else
-                    executeForSourceAndLength<true>(
-                        std::forward<SourceStrings>(strings), GenericValueSource{*column_length}, padding_chars, res_sink);
+                    executeForSourceAndLength<true>(std::forward<SourceStrings>(strings), GenericValueSource{*column_length}, padding_chars, res_sink);
             }
+        }
+
+        template <bool is_actually_utf8, typename SourceStrings, typename SourceLengths>
+        void executeForSourceAndLength(
+            SourceStrings && strings,
+            SourceLengths && lengths,
+            const PaddingChars<is_actually_utf8> & padding_chars,
+            StringSink & res_sink) const
+        {
+            if (is_right_pad)
+                executePad<true>(std::forward<SourceStrings>(strings), std::forward<SourceLengths>(lengths), padding_chars, res_sink);
+            else
+                executePad<false>(std::forward<SourceStrings>(strings), std::forward<SourceLengths>(lengths), padding_chars, res_sink);
         }
 
         template <bool is_right_pad_, bool is_actually_utf8, typename SourceStrings, typename SourceLengths>
@@ -333,19 +314,6 @@ namespace
                         padding_chars.appendTo(res_sink, new_length - current_length);
                 }
             }
-        }
-
-        template <bool is_actually_utf8, typename SourceStrings, typename SourceLengths>
-        void executeForSourceAndLength(
-            SourceStrings && strings,
-            SourceLengths && lengths,
-            const PaddingChars<is_actually_utf8> & padding_chars,
-            StringSink & res_sink) const
-        {
-            if (is_right_pad)
-                executePad<true>(std::forward<SourceStrings>(strings), std::forward<SourceLengths>(lengths), padding_chars, res_sink);
-            else
-                executePad<false>(std::forward<SourceStrings>(strings), std::forward<SourceLengths>(lengths), padding_chars, res_sink);
         }
 
         const char * const function_name;

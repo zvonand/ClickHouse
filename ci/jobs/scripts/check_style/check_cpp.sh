0d888ddef3d73434d7183335de6e3cb642ce7516
Raúl Marín <raul.marin@clickhouse.com>
2026-04-08 19:55:21 +0200

    Replace xargs -i with portable xargs -I{}

--- ci/jobs/scripts/check_style/check_cpp.sh
+++ ci/jobs/scripts/check_style/check_cpp.sh
@@ -332,11 +332,11 @@ QUOTE_EXCLUSIONS=(
 )
 xargs < "$STYLE_TMPDIR/nobase_excluded" grep -P '#include[\s]*(").*(")' "${QUOTE_EXCLUSIONS[@]}" | \
   grep -v -F -e \"config.h\" -e \"config_tools.h\" -e \"SQLGrammar.pb.h\" -e \"out.pb.h\" -e \"clickhouse_grpc.grpc.pb.h\" -e \"delta_kernel_ffi.hpp\" | \
-  xargs -i echo "Found include with quotes in '{}'. Please use <> instead"
+  xargs -I{} echo "Found include with quotes in '{}'. Please use <> instead"
 
 # Forbid using std::shared_mutex and point to the faster alternative
 xargs < "$STYLE_TMPDIR/nobase_excluded" grep 'std::shared_mutex' | \
-  xargs -i echo "Found std::shared_mutex '{}'. Please use DB::SharedMutex instead"
+  xargs -I{} echo "Found std::shared_mutex '{}'. Please use DB::SharedMutex instead"
 
 # Context.h (and a few similar headers) is included in many parts of the
 # codebase, so any modifications to it trigger a large-scale recompilation.

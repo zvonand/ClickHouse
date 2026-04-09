711a7a9ecb65cfce46488eff48874805374588c8
János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>
2026-04-09 16:24:50 +0200

    Fix typo

--- src/Storages/MergeTree/MergeTreePartInfo.h
+++ src/Storages/MergeTree/MergeTreePartInfo.h
@@ -159,7 +159,7 @@ public:
     /// The maximum block number cannot be higher than the maximum sequence number that Keeper can handle.
     /// The sequence number was Int32 originally, it was recently changed to Int64 (https://github.com/ClickHouse/ClickHouse/pull/99120).
     /// To be safe, we keep it as Int32, but more than doubled than the old maximum block number (999999999).
-    /// Once we are sure bigger block numbers don't cause any issues, we can increate it even higher.
+    /// Once we are sure bigger block numbers don't cause any issues, we can increase it even higher.
     static constexpr UInt32 MAX_BLOCK_NUMBER = static_cast<UInt32>(std::numeric_limits<Int32>::max());
     static constexpr std::string_view PATCH_PART_PREFIX = "patch-";
     /// The full prefix of patch part is "patch-<hash>-".

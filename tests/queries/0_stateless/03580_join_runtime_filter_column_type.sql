19e7720bea22a81f28cab9d94f934f8e739ef8f6
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-04-09 09:03:40 +0000

    update test

--- tests/queries/0_stateless/03580_join_runtime_filter_column_type.sql
+++ tests/queries/0_stateless/03580_join_runtime_filter_column_type.sql
@@ -1,4 +1,6 @@
 -- Repro for https://github.com/ClickHouse/ClickHouse/issues/89062
+set enable_analyzer=1;
+
 CREATE TABLE t0 (c0 Int) ENGINE = Memory;
 INSERT INTO TABLE t0 (c0) VALUES (1);
 SELECT 1 FROM t0 WHERE EXISTS (SELECT t0._table) SETTINGS enable_join_runtime_filters = 1, allow_experimental_correlated_subqueries = 1;

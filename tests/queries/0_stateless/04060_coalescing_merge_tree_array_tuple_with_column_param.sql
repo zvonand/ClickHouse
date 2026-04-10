0947fb5bf46f175f374109a6632c660107d6873c
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2026-04-10 13:35:35 +0000

    start

---
+++ tests/queries/0_stateless/04060_coalescing_merge_tree_array_tuple_with_column_param.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS 04060_test;
+
+CREATE TABLE 04060_test
+(
+    key            String,
+    str_col        String,
+    arr_col        Array(String),
+    tuple_col      Tuple(x String, y UInt32),
+    update_column  Nullable(String)
+)
+ENGINE = CoalescingMergeTree(update_column)
+ORDER BY key;
+
+INSERT INTO 04060_test (key, str_col, arr_col, tuple_col)
+VALUES ('k', 'first', ['a', 'b'], ('hello', 42));
+
+INSERT INTO 04060_test (key, update_column)
+VALUES ('k', 'v2');
+
+SELECT str_col, arr_col, tuple_col, update_column
+FROM 04060_test FINAL;
+
+OPTIMIZE TABLE 04060_test FINAL;
+
+SELECT str_col, arr_col, tuple_col, update_column
+FROM 04060_test;
+
+DROP TABLE 04060_test;

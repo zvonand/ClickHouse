5778d6790895dded863e0b744734c20ece55e919
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2026-04-10 19:16:50 -0700

    More HOT_RELOAD

--- src/Coordination/KeeperStorage.cpp
+++ src/Coordination/KeeperStorage.cpp
@@ -4107,7 +4107,6 @@ KeeperResponsesForSessions KeeperStorage<Container>::processLocalRequests(
 
     {
         std::shared_lock lock(storage_mutex);
-        /// TODO: If https://github.com/ClickHouse/ClickHouse/pull/100773 is merged, change this to getDynamicSettings() and add HOT_RELOAD.
         read_thread_pool.execute(tasks.size(), keeper_context->getCoordinationSettings(),
             [&](size_t begin, size_t end)
             {

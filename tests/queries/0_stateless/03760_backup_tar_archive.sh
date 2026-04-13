80191b0a0dbf41c6804e0c5dbeb63bc20a926efd
avogar <avogar@clickhouse.com>
2026-04-13 12:31:33 +0000

    Change test -3760+backup_tar_archive so it can be run in parallel with itself

---
+++ tests/queries/0_stateless/03760_backup_tar_archive.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest, no-encrypted-storage
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+
+$CLICKHOUSE_CLIENT -q "
+DROP TABLE IF EXISTS t0 SYNC;
+DROP TABLE IF EXISTS t1 SYNC;
+
+CREATE TABLE t0 (c1 Int) ENGINE = MergeTree() ORDER BY c1 PARTITION BY (c1 % 6451);
+SET min_insert_block_size_rows = 64, optimize_trivial_insert_select = 1;
+INSERT INTO TABLE t0 (c1) SELECT number FROM numbers(500);
+
+BACKUP TABLE t0 TO Disk('backups', '03760_backup_tar_archive_$CLICKHOUSE_TEST_UNIQUE_NAME.tar') FORMAT Null;
+
+RESTORE TABLE t0 AS t1 FROM Disk('backups', '03760_backup_tar_archive_$CLICKHOUSE_TEST_UNIQUE_NAME.tar') FORMAT Null;
+
+SELECT * FROM t1 ORDER BY c1 LIMIT 10;
+"

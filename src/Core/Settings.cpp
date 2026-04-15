2de3a9df9f31db99a31085379f780029f3e97feb
Nikolai Kochetov <nk@clickhouse.com>
2026-04-15 16:29:34 +0000

    Fix setting descr

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -6011,7 +6011,7 @@ Use query plan for lazy materialization optimization.
     DECLARE(UInt64, query_plan_max_limit_for_lazy_materialization, 10000, R"(Control maximum limit value that allows to use query plan for lazy materialization optimization. If zero, there is no limit.
 )", 0) \
     DECLARE(Bool, query_plan_optimize_lazy_final, false, R"(
-Optimize reading with FINAL from ReplacingMergeTree by building a set of sorting keys and using it for index analysis.
+Optimize reading with FINAL from ReplacingMergeTree by building a set of primary keys and using it for index analysis.
 )", 0) \
     DECLARE(UInt64, max_rows_for_lazy_final, 10000000, R"(
 Maximum number of rows in the set for lazy FINAL optimization. If exceeded, falls back to normal FINAL.

3d4ca194673b65a8ab41774b771e50e3607bdf13
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-04-15 22:58:11 +0200

    Stop using the system compiler-rt libraries and headers, use bundled llvm-project instead

--- cmake/linux/default_libs.cmake
+++ cmake/linux/default_libs.cmake
@@ -3,25 +3,10 @@
 
 set (DEFAULT_LIBS "-nodefaultlibs")
 
-set (BUILTINS_LIBRARY "")
-if (USE_SYSTEM_COMPILER_RT)
-    # We need builtins from Clang
-    execute_process (COMMAND
-        ${CMAKE_CXX_COMPILER} --target=${CMAKE_CXX_COMPILER_TARGET} --print-libgcc-file-name --rtlib=compiler-rt
-        OUTPUT_VARIABLE BUILTINS_LIBRARY
-        COMMAND_ERROR_IS_FATAL ANY
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-endif ()
-
-if (NOT EXISTS "${BUILTINS_LIBRARY}")
-    # In the past we used to fallback into using libgcc, which then required sysroot to include gcc libraries.
-    # Now we build the compiler-rt from source instead so we are independent of the target system's gcc.
-    include (cmake/build_clang_builtin.cmake)
-    build_clang_builtin(${CMAKE_CXX_COMPILER_TARGET} BUILTINS_LIBRARY)
-elseif (SANITIZE STREQUAL undefined)
-    # UBSan support library for C++ is in ubsan_standalone_cxx.a, so we have to include both.
-    string(REPLACE "builtins.a" "ubsan_standalone_cxx.a" EXTRA_BUILTINS_LIBRARY "${BUILTINS_LIBRARY}")
-endif ()
+# Always build compiler-rt from source to avoid depending on the host system's compiler-rt.
+# This ensures consistent behavior regardless of the system compiler version or configuration.
+include (cmake/build_clang_builtin.cmake)
+build_clang_builtin(${CMAKE_CXX_COMPILER_TARGET} BUILTINS_LIBRARY)
 
 option (ENABLE_LLVM_LIBC_MATH "Use math from llvm-libc instead of glibc" ON)
 if (NOT (ARCH_AMD64 OR ARCH_AARCH64))

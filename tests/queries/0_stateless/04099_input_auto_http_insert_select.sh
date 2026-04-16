4982f1b9f2ddce267754b9870dc5b579b090e232
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-04-16 17:37:47 +0000

    Add test case for explicit column list with input(auto) via HTTP

--- tests/queries/0_stateless/04099_input_auto_http_insert_select.sh
+++ tests/queries/0_stateless/04099_input_auto_http_insert_select.sh
@@ -6,10 +6,18 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS t_04099"
+
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE t_04099 (a UInt32, b String, extra String) ENGINE=Memory"
 
 ${CLICKHOUSE_CLIENT} --query="SELECT number::UInt32 AS a, 'val' AS b FROM numbers(3) FORMAT Native" | \
     ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&query=INSERT+INTO+t_04099+SELECT+*,+%27const%27+AS+extra+FROM+input(%27auto%27)+FORMAT+Native" --data-binary @-
+
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM t_04099 ORDER BY a"
+
+${CLICKHOUSE_CLIENT} --query="TRUNCATE TABLE t_04099"
+
+${CLICKHOUSE_CLIENT} --query="SELECT number::UInt32 AS a, 'val' AS b, 'x' AS extra FROM numbers(3) FORMAT Native" | \
+    ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&query=INSERT+INTO+t_04099+(a,b,extra)+SELECT+*+FROM+input(%27auto%27)+FORMAT+Native" --data-binary @-
 
 ${CLICKHOUSE_CLIENT} --query="SELECT * FROM t_04099 ORDER BY a"
 

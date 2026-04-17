bb548165d699fc982fef6ba94e66b47c6ff58eb3
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-04-17 23:08:02 +0000

    Only force setting for EXPLAIN query

--- tests/queries/0_stateless/04099_distinct_aggregate_projection_partial_projection_data.sql
+++ tests/queries/0_stateless/04099_distinct_aggregate_projection_partial_projection_data.sql
@@ -4,10 +4,7 @@
 
 -- { echo }
 
-SET enable_analyzer = 1;
 SET optimize_use_projections = 1;
-SET optimize_move_to_prewhere = 1;
-SET query_plan_optimize_prewhere = 1;
 
 DROP TABLE IF EXISTS test_distinct_proj_partial_projection;
 
@@ -27,15 +24,15 @@ INSERT INTO test_distinct_proj_partial_projection VALUES (3, 30), (4, 40), (4, 4
 SELECT count() FROM test_distinct_proj_partial_projection;
 
 SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection ORDER BY a, b;
-EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection ORDER BY a, b;
+EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection ORDER BY a, b SETTINGS optimize_move_to_prewhere = 1, query_plan_optimize_prewhere = 1;
 
 SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a <= 2 ORDER BY a, b;
-EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a <= 2 ORDER BY a, b;
+EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a <= 2 ORDER BY a, b SETTINGS optimize_move_to_prewhere = 1, query_plan_optimize_prewhere = 1;
 
 SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a >= 4 ORDER BY a, b;
-EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a >= 4 ORDER BY a, b;
+EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a >= 4 ORDER BY a, b SETTINGS optimize_move_to_prewhere = 1, query_plan_optimize_prewhere = 1;
 
 SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a = 3 ORDER BY a, b;
-EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a = 3 ORDER BY a, b;
+EXPLAIN SELECT DISTINCT a, b FROM test_distinct_proj_partial_projection WHERE a = 3 ORDER BY a, b SETTINGS optimize_move_to_prewhere = 1, query_plan_optimize_prewhere = 1;
 
 DROP TABLE test_distinct_proj_partial_projection;

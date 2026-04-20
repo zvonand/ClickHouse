257acfc5a8efa3ddba86b46a0557d4a9506210b1
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-04-20 06:49:54 +0000

    Update tests

--- tests/queries/0_stateless/04106_negative_limit_by_distributed_large_boundary.sql
+++ tests/queries/0_stateless/04106_negative_limit_by_distributed_large_boundary.sql
@@ -8,27 +8,27 @@ INSERT INTO test SELECT number FROM numbers(200000);
 
 -- LIMIT -3 BY g (InOrder)
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY g, id LIMIT -3 BY g;
--- LIMIT -3 BY g (Common)
+-- LIMIT -3 BY g
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY id LIMIT -3 BY g;
 
 -- LIMIT -3 OFFSET -50 BY g (InOrder)
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY g, id LIMIT -3 OFFSET -50 BY g;
--- LIMIT -3 OFFSET -50 BY g (Common)
+-- LIMIT -3 OFFSET -50 BY g
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY id LIMIT -3 OFFSET -50 BY g;
 
 -- LIMIT -3 OFFSET 50 BY g (InOrder)
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY g, id LIMIT -3 OFFSET 50 BY g;
--- LIMIT -3 OFFSET 50 BY g (Common)
+-- LIMIT -3 OFFSET 50 BY g
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY id LIMIT -3 OFFSET 50 BY g;
 
 -- LIMIT 3 OFFSET -50 BY g (InOrder)
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY g, id LIMIT 3 OFFSET -50 BY g;
--- LIMIT 3 OFFSET -50 BY g (Common)
+-- LIMIT 3 OFFSET -50 BY g
 SELECT id, intDiv(id, 50000) AS g FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY id LIMIT 3 OFFSET -50 BY g;
 
 -- INT_MIN LIMIT (InOrder)
 SELECT count(), sum(id) FROM (SELECT id FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY intDiv(id, 50000), id LIMIT -9223372036854775808 BY intDiv(id, 50000));
--- INT_MIN LIMIT (Common)
+-- INT_MIN LIMIT
 SELECT count(), sum(id) FROM (SELECT id FROM remote('127.0.0.1,127.0.0.2', currentDatabase(), test) ORDER BY id LIMIT -9223372036854775808 BY intDiv(id, 50000));
 
 DROP TABLE test;

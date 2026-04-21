5e8bc7663c108b06896ee802684f59b77e839b7b
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-04-21 17:32:58 +0000

    Fix data race in dictionaries

---
+++ src/Dictionaries/InvalidateQueryResponse.cpp
@@ -0,0 +1,27 @@
+#include <Dictionaries/InvalidateQueryResponse.h>
+
+namespace DB
+{
+
+InvalidateQueryResponse::InvalidateQueryResponse(const InvalidateQueryResponse & other)
+{
+    std::lock_guard lock(other.mutex);
+    response = other.response;
+}
+
+bool InvalidateQueryResponse::updateAndCheckModified(const std::string & new_response)
+{
+    std::lock_guard lock(mutex);
+    if (response == new_response)
+        return false;
+    response = new_response;
+    return true;
+}
+
+std::string InvalidateQueryResponse::get() const
+{
+    std::lock_guard lock(mutex);
+    return response;
+}
+
+}
---
+++ src/Dictionaries/InvalidateQueryResponse.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <mutex>
+#include <string>
+
+namespace DB
+{
+
+/// Thread-safe holder for the last value returned by a dictionary source's invalidate query.
+class InvalidateQueryResponse
+{
+public:
+    InvalidateQueryResponse() = default;
+    InvalidateQueryResponse(const InvalidateQueryResponse & other);
+    InvalidateQueryResponse & operator=(const InvalidateQueryResponse &) = delete;
+
+    bool updateAndCheckModified(const std::string & new_response);
+    std::string get() const;
+
+private:
+    mutable std::mutex mutex;
+    std::string response;
+};
+
+}

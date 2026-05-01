5f0bf1721d6387a913f6c69318c909103291dcc1
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2026-05-01 05:29:46 +0000

    enable analyzer for test

--- tests/queries/0_stateless/04105_in_non_constant_tuple_type_promotion.sql
+++ tests/queries/0_stateless/04105_in_non_constant_tuple_type_promotion.sql
@@ -2,6 +2,8 @@
 -- elements down to the LHS type, which would silently overflow (issue #103055)
 -- Example: `toUInt8(232) IN (1000, number)` used to return 1 because CAST(1000, 'UInt8') = 232
 
+SET enable_analyzer = 1;
+
 SELECT toUInt8(232) IN (1000, 0);
 SELECT toUInt8(232) IN (1000, number) FROM numbers(1);
 SELECT toUInt8(0) IN (256, 999);

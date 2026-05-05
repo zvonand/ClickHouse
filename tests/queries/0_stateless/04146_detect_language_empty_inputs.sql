14cacb0db257ee15f83423fceb4ea479effa8597
Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>
2026-05-05 10:24:44 -0400

    Delete tests/queries/0_stateless/04146_detect_language_empty_inputs.sql

--- tests/queries/0_stateless/04146_detect_language_empty_inputs.sql
+++
@@ -1,9 +0,0 @@
--- Tags: no-fasttest
--- Tag no-fasttest: depends on cld2 and nlp-data
-
-SET allow_experimental_nlp_functions = 1;
-
-SELECT detectLanguage('');
-SELECT detectLanguage(toString(reinterpretAsUInt8(0)));
-SELECT mapSort(detectLanguageMixed(''));
-SELECT mapSort(detectLanguageMixed(toString(reinterpretAsUInt8(0))));

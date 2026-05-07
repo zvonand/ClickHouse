3ae967c9a44adf557ec188fea7e46766f20524ab
Desel72 <desel72@users.noreply.github.com>
2026-05-07 22:34:31 +0200

    test(parser): avoid alias dependency in values regression
    Use generated column names for the SQL-standard  sanity query in the regression test, so it also works in old-analyzer stateless jobs. The test still covers parenthesized subscripts on a column named .
    
    CI: https://s3.amazonaws.com/clickhouse-test-reports/json.html?PR=104312&sha=5a19458a55e1471fef3845ccb61e3d0a96a70026&name_0=PR&name_1=Stateless%20tests%20%28amd_llvm_coverage%2C%20old%20analyzer%2C%20s3%20storage%2C%20DatabaseReplicated%2C%20WasmEdge%2C%20parallel%29
    
    PR: https://github.com/ClickHouse/ClickHouse/pull/104312

--- tests/queries/0_stateless/04209_values_keyword_column_subscript.sql
+++ tests/queries/0_stateless/04209_values_keyword_column_subscript.sql
@@ -2,4 +2,4 @@ SELECT (values['a']) AS x FROM (SELECT map('a', 42) AS values);
 SELECT (values['a'] - 1) AS x FROM (SELECT map('a', 42) AS values);
 SELECT (VALUES['a']) AS x FROM (SELECT map('a', 43) AS VALUES);
 
-SELECT * FROM (VALUES (1, 'a'), (2, 'b')) AS t(id, value) ORDER BY id;
+SELECT * FROM (VALUES (1, 'a'), (2, 'b')) ORDER BY c1;

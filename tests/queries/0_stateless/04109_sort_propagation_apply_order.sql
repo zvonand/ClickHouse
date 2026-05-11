0ad2a69f62d7908e1ca6752280ca719f40297ec7
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-05-11 14:56:10 +0000

    Add test for negative limit by case

--- tests/queries/0_stateless/04109_sort_propagation_apply_order.sql
+++ tests/queries/0_stateless/04109_sort_propagation_apply_order.sql
@@ -21,3 +21,12 @@ FROM
     LIMIT 1 BY g
 )
 ORDER BY g, x;
+
+SELECT g, x
+FROM
+(
+    (SELECT number AS g, number AS x FROM numbers(2) ORDER BY g, x)
+    UNION ALL
+    (SELECT number AS g, number AS x FROM numbers(2) ORDER BY g, x)
+)
+LIMIT -1 BY g;

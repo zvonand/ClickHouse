8d7f4a5d98bc47567a66ac850caa109492f63f80
Max Kainov <max.kainov@clickhouse.com>
2026-05-13 17:10:28 +0200

    add digest config for libfuzzer

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -1467,6 +1467,9 @@ class JobConfigs:
         runs_on=RunnerLabels.ARM_MEDIUM,
         command="python3 ./ci/jobs/libfuzzer_test_check.py 'libFuzzer tests'",
         requires=[ArtifactNames.ARM_FUZZERS, ArtifactNames.FUZZERS_CORPUS],
+        digest_config=Job.CacheDigestConfig(
+            include_paths=["./ci/jobs/libfuzzer_test_check.py"],
+        ),
     )
     toolchain_build_jobs = Job.Config(
         name=JobNames.BUILD_TOOLCHAIN,
